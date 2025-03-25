978f734f3d411daad924129eccac3a48ccf2d807
Carter Butaud <carterbutaud@gmail.com>
2025-03-25 13:21:05 -0700

    basic styles

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -13,21 +13,26 @@ export const Header = () => {
 
   return (
     <header>
-      <h3>Prepare To Board</h3>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-      <div>
+      <div className="start">
+        <div className="brand">
+          <img src="/logo.png" alt="Logo" />
+          <h3>Prepare To Board</h3>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">{isAuthenticated ? "Home" : "Welcome"}</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="end">
         {isAuthenticated && (
           <>
-            <h1>Hello, {me?.profile?.name}</h1>
+            <h3>Hello, {me?.profile?.name}</h3>
             <button onClick={onLogOut}>Log out</button>
           </>
         )}
