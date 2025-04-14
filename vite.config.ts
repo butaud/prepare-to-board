a6113bd4a4b7f61637e0759a9950643d932b5274
Carter Butaud <carterbutaud@gmail.com>
2025-04-14 01:47:10 -0700

    defined vite base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/prepare-to-board/',
   plugins: [react()],
   test: {
     globals: true,
